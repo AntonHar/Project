1484ba99d91035784eb4c0c23988f44c05713468
Anton Harbatovich <Toha8118934@gmail.com>
2020-11-30 18:46:54 +0300

    1

---
+++ Bog.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+public class Bog {
+    public static void main(String[] args) {
+        /* int ImputValue;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Imput value");
+        ImputValue = scanner.nextInt();
+        if (ImputValue <=9 && ImputValue > 0)            System.out.println("one-digit number");
+        else if (ImputValue <= 99 && ImputValue > 0)            System.out.println("two-digit number");
+        else if (ImputValue <= 999 && ImputValue > 0) System.out.println("two-digit number");
+        else System.out.println("false");
+        scanner.close(); */
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.print("a");
+        int a = scanner.nextInt();
+        System.out.print("b");
+        int b = scanner.nextInt();
+        System.out.print("c");
+        int c = scanner.nextInt();
+        if (a <= b + c && b <= a + c && c <= b + a) System.out.println("exist");
+        else System.out.println("does not exist");
+        scanner.close();*/
+        // Задание 3
+       /* Scanner scanner = new Scanner(System.in);
+        System.out.print("a");
+        int a = scanner.nextInt();
+        if (a >0) {                System.out.println("Задание 3 - " + (a+1));
+        }; if (a < 0){ System.out.println("Задание 3 -" + (a - 2));
+        }; if (a == 0){ System.out.println("Задание 3 - " + (a = 10));};*/
+
+        // Задание 4
+       /* Scanner scanner = new Scanner(System.in);
+        System.out.print("a ");
+        int a = scanner.nextInt();
+        System.out.print("b ");
+        int b = scanner.nextInt();
+        System.out.print("c ");
+        int c = scanner.nextInt();
+        if (a > 0){ a = 1; }
+        else { a = 0;}
+        if (b >0){  b = 1;      }
+        else { b = 0;}
+        if (c > 0){  c = 1;}
+        else {c =0;}
+        System.out.print("Количество положительных - " + (a + b + c));*/
+
+        // Задание 5
+       /* Scanner scanner = new Scanner(System.in);
+        System.out.print("a ");
+        int a = scanner.nextInt();
+        System.out.print("b ");
+        int b = scanner.nextInt();
+        System.out.print("c ");
+        int c = scanner.nextInt();
+        int d = a;
+        int e = b;
+        int f = c;
+        if (d < 0) { d = 1; a = 0;
+        } else { if (d > 0) { a = 1; d = 0;
+            } else { d = 0; a = 0;}
+        }
+        if (e < 0) { e = 1; b = 0;
+        } else { if (e > 0) { b = 1; e = 0;
+        } else { e = 0; b = 0;}
+        }
+        if (f < 0) {f = 1; c = 0;
+        } else { if (f > 0) { c = 1; f = 0;
+        } else { f = 0; c = 0;}
+        }
+        System.out.println("Количество положительных чисел - " + (a+ b + c));
+            System.out.print("Количество отрицательных чисел - " + (d + e + f));*/
+
+        // Задание 6
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.print("a ");
+        int a = scanner.nextInt();
+        System.out.print("b ");
+        int b = scanner.nextInt();
+        {
+            if (a > b) System.out.print("Большее число - " + a);
+            else System.out.print("Большее число - " + b);*/
+
+        // Задание 7
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a ");
+        int a = scanner.nextInt();
+        String b = " программист";
+        if (a == 0) {
+            System.out.print(a + b + "ов");
+        } else {
+            if (a == 1) {
+                System.out.print(a + b);
+            } else {
+                if (a < 5) {
+                    System.out.print(a + b + "а");
+                } else {
+                    System.out.print(a + b + "ов");
+                }
+
+            }
+        }
+    }
+}
